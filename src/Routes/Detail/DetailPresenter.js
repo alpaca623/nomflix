cc6dc6995d479fb9c6814df326c0b7dc8df9ecdb
alpaca <alpaca@alpacaui-MacBookPro.local>
2019-03-10 15:09:42 +0900

    6.8 Detail Container part One

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,13 +1,70 @@
 import React from "react";
 import styled from "styled-components";
 import propTypes from "prop-types";
+import Loader from "Components/Loader";
 
-const SearchPresenter = ({ result, error, loading }) => null;
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  padding:50px;
+`;
+
+const Backdrop = styled.div`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-image: url(${props => props.bgImg});
+  background-size: cover;
+  background-position: center center;
+  filter: blur(3px);
+  opacity: 0.3;
+  z-index: 0;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+`;
+
+const Cover = styled.div`
+  width: 30%;
+  background-image: url(${props => props.bgImg});
+  background-position: center center;
+  background-size: cover;
+  height: 100%;
+  border-radius:5px;
+`;
+
+const SearchPresenter = ({ result, error, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <Backdrop
+        bgImg={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+      />
+      <Content>
+        <Cover
+          bgImg={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+              : require("../../assets/noImage.png")
+          }
+        />
+      </Content>
+    </Container>
+  );
 
 SearchPresenter.propTypes = {
-  result: propTypes.array,
-  error: propTypes.String,
-  loading: propTypes.bool,
+  result: propTypes.object,
+  error: propTypes.string,
+  loading: propTypes.bool
 };
 
 export default SearchPresenter;
